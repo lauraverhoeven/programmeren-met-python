94ff654a4537f34a047b34a4bdb1ffad1775f3bd
Verhoeven,L. (Laura) <l.verhoeven2@students.uu.nl>
2020-03-02 12:17:41 +0100

    Add new file

---
+++ src/main.py
@@ -0,0 +1,15 @@
+# main program file, handling the interaction with the user
+
+# load CBS data setprint("To do: Load CBS data set.")
+
+# run program in loop until user chooses to exit
+while True:    
+# print options to user:    
+
+choice = input("""What do you want to do?    
+1\tAnalyze how the reported ICT skills differ between groups.    
+2\tAnalyze how the reported skill levels have changed over time.    
+3\tAnalyze which skills most people have, and which not.    
+4\tExit program.    """)    
+
+# evaluate user choice and proceed accordi
